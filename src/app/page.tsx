fe89ae3d5f38b4e48edac1546ffb35d7efe8acf6
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-09-19 00:40:52 +0500

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import styles from "./page.module.css";
 import Header from "@/components/Header/Header";
 import Products from "@/components/Products/Products";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("https://api.escuelajs.co/api/v1/products", {
+    cache: "no-store",
+  });
+  const products = await response.json();
+  console.log(products);
+
   return (
     <>
       <Header />
