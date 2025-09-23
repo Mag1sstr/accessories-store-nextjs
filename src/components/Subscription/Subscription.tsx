9a7cf530583d26ee9e3d106212c79f8e9b5ae3f3
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-09-24 01:17:20 +0500

    update Subscription

--- src/components/Subscription/Subscription.tsx
+++ src/components/Subscription/Subscription.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./Subscription.module.css";
 
 function Subscription() {
@@ -5,12 +6,28 @@ function Subscription() {
     <div className={styles.wrapper}>
       <div className="container">
         <div className={styles.row}>
-          <div className={styles.image}></div>
+          <div className={styles.image}>
+            <Image
+              src="/assets/subscription/img.png"
+              alt="phone"
+              width={484}
+              height={371}
+            />
+          </div>
           <div className={styles.content}>
             <h2>Подписка на рассылку акций!</h2>
             <p>Подпишитесь на рассылку, чтобы узнавать о всех акциях первым</p>
             <form className={styles.form}>
               <h3>Скидка 10%</h3>
+              <p>на первую покупку за подписку</p>
+              <div>
+                <input
+                  className={styles.input}
+                  type="number"
+                  placeholder="+ 7"
+                />
+                <button className={styles.btn}>подписаться</button>
+              </div>
             </form>
           </div>
         </div>
