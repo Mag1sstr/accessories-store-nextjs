269cf43bd9008d3d03fb7ff8bc2194b198fd82f2
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-10-04 23:45:20 +0500

    Create single product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import { useParams } from "next/navigation";
 
 export default function SingleProduct() {
   const { id } = useParams();
   return (
     <>
-      <p>ID: {id}</p>
+      <Header />
+      <Footer />
     </>
   );
 }
