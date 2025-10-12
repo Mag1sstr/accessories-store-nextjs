e7f472eec746de73cb2ac1284e8e34958a9c2b6a
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-10-13 01:10:10 +0500

    update ProductCard

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import FiltersProducts from "@/components/FiltersProducts/FiltersProducts";
 import Footer from "@/components/Footer/Footer";
+
 export default async function Products() {
   const response = await fetch("https://api.escuelajs.co/api/v1/products", {
     cache: "no-store",
