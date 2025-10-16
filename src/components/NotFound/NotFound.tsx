3c04eb85633925afb762a53e3885857d69c16f8d
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-10-17 02:24:10 +0500

    update NotFoundPage

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,4 +1,6 @@
 import styles from "./NotFound.module.css";
+import imageTel from "../../../public/assets/notfound/404.png";
+import Image from "next/image";
 function NotFound() {
   return (
     <section className={styles.wrapper}>
@@ -6,6 +8,9 @@ function NotFound() {
         <h1>Ошибка 404</h1>
         <p>Кажется, такой страницы больше не существует.</p>
         <button></button>
+        <div className={styles.image}>
+          <Image src={imageTel} alt="404" width={456} height={456} />
+        </div>
       </div>
     </section>
   );
