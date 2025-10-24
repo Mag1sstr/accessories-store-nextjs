7397bae89563f1ecb62790b7bea5e7e8f22094a0
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-10-25 01:41:30 +0500

    update Guarantee page

--- src/app/guarantee/page.tsx
+++ src/app/guarantee/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer/Footer";
+import Guarantee from "@/components/Guarantee/Guarantee";
 import Header from "@/components/Header/Header";
 
 export default function Page() {
   return (
     <>
       <Header />
+      <Guarantee />
       <Footer />
     </>
   );
