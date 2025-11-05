2ba7e52f9c9e79ed3f6c8b134710623b19da71a1
Daniil K <danilkarachev234@mail.ru>
2025-11-06 00:27:57 +0500

    update CreditPage

--- src/components/CreditPage/CreditPage.tsx
+++ src/components/CreditPage/CreditPage.tsx
@@ -15,6 +15,27 @@ function CreditPage() {
             <p>в кредит, за 30 минут без похода в банк</p>
           </div>
         </div>
+        <div className={styles.steps}>
+          <h3>Получите кредит в 4 шага</h3>
+          <div className={styles.stepsRow}>
+            <div className={styles.step}>
+              <div className={styles.stepNum}>1</div>
+              <p>Выберите товар и оформите заказ с оплатой в кредит</p>
+            </div>
+            <div className={styles.step}>
+              <div className={styles.stepNum}>2</div>
+              <p>Согласуйте условия кредитования с нашим менеджером</p>
+            </div>
+            <div className={styles.step}>
+              <div className={styles.stepNum}>3</div>
+              <p>Получите решение от банков и выберите лучшие условия</p>
+            </div>
+            <div className={styles.step}>
+              <div className={styles.stepNum}>4</div>
+              <p>Ожидайте доставку вашего заказа (или самовывоз)</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
