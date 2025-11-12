f2c32b73c15f05b5e36e8f4639a78d07890f36ed
Daniil K <danilkarachev234@mail.ru>
2025-11-13 03:18:11 +0500

    create InputField

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,39 @@
+import styles from "./InputField.module.css";
+import { HTMLInputTypeAttribute } from "react";
+import { UseFormRegister, UseFormRegisterReturn } from "react-hook-form";
+import checkImg from "../../../public/assets/icons/check.png";
+import { IFields } from "../RegModal/RegModal";
+
+interface IProps {
+  type?: HTMLInputTypeAttribute;
+  register?: UseFormRegisterReturn;
+  errors?: boolean;
+  placeholder?: string;
+  getValues?: boolean;
+}
+
+function InputField({
+  type = "text",
+  register,
+  errors,
+  placeholder,
+  getValues,
+}: IProps) {
+  return (
+    <div className={styles.inputBlock}>
+      <input
+        className={`${styles.field} ${errors && styles.err}`}
+        type={type}
+        placeholder={placeholder}
+        {...register}
+      />
+      <img
+        className={`${styles.checkIcon} ${!errors && getValues && styles.not}`}
+        src={checkImg.src}
+        alt="check"
+      />
+    </div>
+  );
+}
+
+export default InputField;
--- src/components/RegModal/RegModal.tsx
+++ src/components/RegModal/RegModal.tsx
@@ -6,15 +6,18 @@ import { useEffect, useState } from "react";
 import { useLoginUserMutation, useRegUserMutation } from "@/api/api";
 import ModalSuccess from "../ModalSuccess/ModalSuccess";
 import ModalWrapper from "../ModalWrapper/ModalWrapper";
-import checkImg from "../../../public/assets/icons/check.png";
+import InputField from "../InputField/InputField";
+import { useAppDispatch } from "@/store/store";
+import { setToken } from "@/store/slices/authSlice";
 
-interface IFields {
+export interface IFields {
   name: string;
   email: string;
   password: string;
 }
 
 function RegModal() {
+  const dispatch = useAppDispatch();
   const [regSuccess, setRegSuccess] = useState(false);
 
   const {
@@ -25,7 +28,8 @@ function RegModal() {
   } = useForm<IFields>({ mode: "onChange" });
   const { openRegModal, setOpenRegModal, setOpenLoginModal } = useModals();
   const [regUser, { data, isSuccess }] = useRegUserMutation();
-  const [loginUser] = useLoginUserMutation();
+  const [loginUser, { data: loginData, isSuccess: isLoginSuccess }] =
+    useLoginUserMutation();
 
   const submit: SubmitHandler<IFields> = (data) => {
     regUser({ ...data, avatar: "https://picsum.photos/800" });
@@ -49,6 +53,12 @@ function RegModal() {
     return () => clearTimeout(timer);
   }, [isSuccess]);
 
+  useEffect(() => {
+    if (isLoginSuccess && loginData.access_token) {
+      dispatch(setToken(loginData.access_token));
+    }
+  }, [isLoginSuccess]);
+
   return (
     <ModalWrapper isOpen={openRegModal} setIsOpen={setOpenRegModal}>
       <form
@@ -58,7 +68,7 @@ function RegModal() {
       >
         <ModalSuccess isActive={regSuccess} text="Вы зарегистрировались!" />
         <h2>Создание аккаунта</h2>
-        <div className={styles.inputBlock}>
+        {/* <div className={styles.inputBlock}>
           <input
             className={`${styles.field} ${errors.name && styles.err}`}
             type="text"
@@ -74,8 +84,37 @@ function RegModal() {
             src={checkImg.src}
             alt="check"
           />
-        </div>
-        <div className={styles.inputBlock}>
+        </div> */}
+        <InputField
+          placeholder="Ваше имя"
+          register={register("name", {
+            required: true,
+          })}
+          errors={!!errors.name}
+          getValues={!!getValues("name")}
+        />
+        <InputField
+          placeholder="Почта"
+          register={register("email", {
+            required: true,
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Неверный email",
+            },
+          })}
+          errors={!!errors.email}
+          getValues={!!getValues("email")}
+        />
+        <InputField
+          placeholder="Пароль"
+          register={register("password", {
+            required: true,
+            minLength: 5,
+          })}
+          errors={!!errors.password}
+          getValues={!!getValues("password")}
+        />
+        {/* <div className={styles.inputBlock}>
           <input
             className={`${styles.field} ${errors.email && styles.err}`}
             type="text"
@@ -95,8 +134,8 @@ function RegModal() {
             src={checkImg.src}
             alt="check"
           />
-        </div>
-        <input
+        </div> */}
+        {/* <input
           className={`${styles.field} ${errors.password && styles.err}`}
           type="password"
           placeholder="Пароль"
@@ -104,7 +143,7 @@ function RegModal() {
             required: true,
             minLength: 5,
           })}
-        />
+        /> */}
         <button type="submit" className={styles.btn}>
           войти
         </button>
