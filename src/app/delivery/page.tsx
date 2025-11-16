6f7a0c77b8c01a7aca5272fa1b5fa9c0477b6a87
Daniil K <danilkarachev234@mail.ru>
2025-11-17 02:58:52 +0500

    create DeliveryPage

---
+++ src/app/delivery/page.tsx
@@ -0,0 +1,15 @@
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
+import DeliveryPage from "@/components/DeliveryPage/DeliveryPage";
+import Header from "@/components/Header/Header";
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Breadcrumbs />
+      <DeliveryPage />
+    </>
+  );
+}
+
+export default Page;
