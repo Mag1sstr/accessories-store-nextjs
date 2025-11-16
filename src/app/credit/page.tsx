8ba92ccb2c1d1b032b80c09cefb573b077ac5570
Daniil K <danilkarachev234@mail.ru>
2025-11-17 00:20:35 +0500

    update Credit page

--- src/app/credit/page.tsx
+++ src/app/credit/page.tsx
@@ -1,11 +1,15 @@
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import CreditPage from "@/components/CreditPage/CreditPage";
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
 export default function Page() {
   return (
     <>
       <Header />
+      <Breadcrumbs />
       <CreditPage />
+      <Footer />
     </>
   );
 }
