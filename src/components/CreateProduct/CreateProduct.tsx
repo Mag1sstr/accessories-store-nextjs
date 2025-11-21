1d16244e9b370634b54fbe1ac44891d69e0b675b
Daniil K <danilkarachev234@mail.ru>
2025-11-21 15:31:16 +0500

    update CreateProduct

--- src/components/CreateProduct/CreateProduct.tsx
+++ src/components/CreateProduct/CreateProduct.tsx
@@ -1,6 +1,13 @@
 import InputField from "../InputField/InputField";
 import styles from "./CreateProduct.module.css";
 
+interface IInputs {
+  title: string;
+  price: number;
+  description: string;
+  categoryId: number;
+  images: string[];
+}
 function CreateProduct() {
   return (
     <section className={styles.wrapper}>
@@ -8,10 +15,14 @@ function CreateProduct() {
         <form className={styles.form}>
           <h2 className={styles.title}>Добавить новый продукт</h2>
           <InputField placeholder="Название" />
-          <InputField placeholder="Цена" />
+          <InputField placeholder="Цена" type="number" />
           <InputField placeholder="Описание" />
-          <InputField placeholder="ID" />
-          <input type="file" />
+          <InputField placeholder="ID" type="number" />
+          <label htmlFor="input__file" className={styles.labelFile}>
+            Выберите изображение
+          </label>
+          <input type="file" id="input__file" className={styles.inputFile} />
+          <button className={styles.btn}>Добавить</button>
         </form>
       </div>
     </section>
