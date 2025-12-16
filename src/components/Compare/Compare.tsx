8598b70bc0b0739e5eb7da07b0f4422dcb478f91
Daniil K <danilkarachev234@mail.ru>
2025-12-17 02:41:17 +0500

    create Compare page

---
+++ src/components/Compare/Compare.tsx
@@ -0,0 +1,13 @@
+import styles from "./Compare.module.css";
+
+function Compare() {
+  return (
+    <section>
+      <div className="container">
+        <div></div>
+      </div>
+    </section>
+  );
+}
+
+export default Compare;
