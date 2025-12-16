633297033047b3f3429d7c55160ae077fadc00b6
Daniil K <danilkarachev234@mail.ru>
2025-12-16 23:31:45 +0500

    create Policy

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,17 @@
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Policy from "@/components/Policy/Policy";
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Breadcrumbs />
+      <Policy />
+      <Footer />
+    </>
+  );
+}
+
+export default Page;
