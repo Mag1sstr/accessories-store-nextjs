74cca3c5794b333c4fdd48e71bd866845d0b96f5
Daniil K <danilkarachev234@mail.ru>
2025-12-27 04:45:44 +0500

    update compareSlice

--- src/store/slices/compareSlice.ts
+++ src/store/slices/compareSlice.ts
@@ -1,13 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { IProducts } from "@/types/interfaces";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {};
+interface ICompareState {
+  compareData: IProducts[];
+}
+const initialState: ICompareState = {
+  compareData: [],
+};
 
 export const compareSlice = createSlice({
   name: "compareSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCompare(state, action) {
+      if (state.compareData.some((el) => el.id === action.payload.id)) return;
+      state.compareData.push(action.payload);
+      localStorage.setItem("compare", JSON.stringify(state.compareData));
+    },
+    deleteCompareItem(state, action: PayloadAction<number>) {
+      state.compareData = state.compareData.filter(
+        (el) => el.id !== action.payload
+      );
+      localStorage.setItem("compare", JSON.stringify(state.compareData));
+    },
+  },
 });
 
-export const {} = compareSlice.actions;
+export const { addToCompare, deleteCompareItem } = compareSlice.actions;
 
 export default compareSlice.reducer;
