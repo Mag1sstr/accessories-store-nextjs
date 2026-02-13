d53a3b04612568d3da12e8b973c221bb6a55dd78
Daniil K <danilkarachev234@mail.ru>
2026-02-13 23:44:43 +0500

    update NavLink.tsx

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,12 +1,23 @@
+import styles from "./NavLink.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface IProps {
   href: string;
   children: React.ReactNode;
+  className?: string;
 }
 
-function NavLink({ href }: IProps) {
-  return <Link href={href}></Link>;
+function NavLink({ href, children, className }: IProps) {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={href}
+      className={`${className} ${href === pathname && styles.active}`}
+    >
+      {children}
+    </Link>
+  );
 }
 
 export default NavLink;
