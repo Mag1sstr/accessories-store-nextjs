b3c74e9634930f614832eba8ebafd223ece66347
Daniil K <danilkarachev234@mail.ru>
2026-02-14 23:33:57 +0500

    update Policy metadata

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -2,6 +2,11 @@ import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Policy from "@/components/Policy/Policy";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Политика возрата",
+};
 
 function Page() {
   return (
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,10 +2,27 @@ import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import SingleProductPage from "@/components/SingleProductPage/SingleProductPage";
+import { IProducts } from "@/types/interfaces";
+import { Metadata } from "next";
 
 interface PageProps {
   params: {
     id: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { id } = await params;
+
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    cache: "no-store",
+  });
+  const product: IProducts = await res.json();
+
+  return {
+    title: product.title ?? "Страница продукта",
   };
 }
 
@@ -15,7 +32,7 @@ export default async function SingleProduct({ params }: PageProps) {
     `https://api.escuelajs.co/api/v1/products/${id}`,
     {
       cache: "no-store",
-    }
+    },
   );
 
   const product = await response.json();
