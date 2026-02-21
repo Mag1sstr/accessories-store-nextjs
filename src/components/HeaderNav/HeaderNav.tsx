a6d43da0f4d1ea5df7c199b82d79134513ef9b34
Daniil K <danilkarachev234@mail.ru>
2026-02-22 03:55:39 +0500

    update User

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -27,7 +27,6 @@ function HeaderNav() {
   };
 
   const width = useWindowWidth();
-  console.log(width);
 
   return (
     <>
@@ -51,7 +50,7 @@ function HeaderNav() {
             {width && width <= 768 && (
               <Search mobile className={styles.search} />
             )}
-            {/* <CategoryList /> */}
+            <CategoryList />
             <User />
             <BurgerButton />
           </div>
